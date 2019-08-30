e1cd251d74e68330705720d9368ffacd6cf338ca
unknown <glastondsouza97@gmail.com>
2019-08-30 20:13:09 +0530

    Floating Elements Done

--- script/function.js
+++ script/function.js
@@ -37,13 +37,24 @@ $(window).scroll(function() {
     $(".window-tint").css({ opacity: opacity });
   }
 
-  // if(wScroll > $('.large-window').offset().top - $(window).height()){
+  //  Flying Elements
+  if (wScroll > $(".blog-posts").offset().top - $(window).height()) {
+    var offset = Math.min(
+      0,
+      wScroll - $(".blog-posts").offset().top + $(window).height() - 350
+    ).toFixed();
 
-  //     $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
+    $(".post-1").css({
+      transform: "translate(" + offset + "px, " + Math.abs(offset * 0.2) + "px)"
+    });
 
-  //     var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
-
-  //     $('.window-tint').css({'opacity': opacity});
-
-  //   }
+    $(".post-3").css({
+      transform:
+        "translate(" +
+        Math.abs(offset) +
+        "px, " +
+        Math.abs(offset * 0.2) +
+        "px)"
+    });
+  }
 });
